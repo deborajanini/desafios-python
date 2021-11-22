a8f481787082719b23adc6887df613ad49bd6741
Débora Janini <deborajaninicg@gmail.com>
2021-11-22 18:08:18 -0300

    Desafio 3

--- desafio3.py
+++ desafio3.py
@@ -1,11 +1,13 @@
 #Desafio 3
 #Débora Janini
 #Area do circulo
+
+#area = round (A,4)
+#print (f"A={area}")
 n = 3.14159
 #entrada do raio
 R = float(input())
 #Calculo da area
 A = (n*(R**2))
-#area = round (A,4)
-#print (f"A={area}")
+
 print(f"A=%.4f" % (A))
