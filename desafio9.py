0e543c63269207d478f4074909574b08ccdc3b6b
Débora Janini <deborajaninicg@gmail.com>
2021-11-23 16:13:14 -0300

    Desafio 9

---
+++ desafio9.py
@@ -0,0 +1,11 @@
+#Desafio9
+#Débora Janini
+
+numer= int(input())
+hour= int(input())
+rece= float(input())
+
+print('NUMBER =', numer)
+print('SALARY = U$', f'{(hour*rece):.2f}')
+
+#esse último print deve-se ao fato de ser int*float
