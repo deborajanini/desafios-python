af74a5dd5bfd15cd5d18a1bdad1a774d1a99faaf
Débora Janini <deborajaninicg@gmail.com>
2021-11-23 15:34:30 -0300

    Desafio 6

---
+++ desafio6.py
@@ -0,0 +1,13 @@
+#Desafio 6
+#Débora Janini
+#média
+
+A = float(input())
+B = float(input())
+
+#A_formatado = "{:.2f}".format(a)
+#B_formatado = "{:.2f}".format(b)
+
+MEDIA= (A*3.5+B*7.5)/11.0
+
+print (f"MEDIA = %.5f" % (MEDIA))
