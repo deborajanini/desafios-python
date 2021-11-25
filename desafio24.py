5942a73b3168294445a1de08bb004a2d8c1dadcf
Débora Janini <deborajaninicg@gmail.com>
2021-11-25 17:53:32 -0300

    Desafio 24

---
+++ desafio24.py
@@ -0,0 +1,9 @@
+#Desafio 24
+#Débora Janini
+
+#range começando em 1, deu errado,
+#em zero incluiu o zero
+#pelo visto o range inclui o primeiro e exclui
+# o ultimo e considera o primeiro para pulos
+for numero in range(2, 101, 2): #alcance de 2 a 101 em passo de 2
+    print(numero)
