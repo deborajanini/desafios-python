4248bef8c04e0b53e26e2215896203c902c8a600
Débora Janini <deborajaninicg@gmail.com>
2021-11-26 16:28:04 -0300

    Desafio 33

---
+++ desafio33.py
@@ -0,0 +1,9 @@
+#Desafio 33
+#Debora Janini
+
+N = int(input())
+
+for i in range(1,11): #lembrando que considera o primeiro
+#e desconsidera o último
+    res = i * N
+    print (f"{i} x {N} = {res}")
