1b7285eef1da7bc95fb3dac2a94994c14a902767
Débora Janini <deborajaninicg@gmail.com>
2021-11-26 16:43:58 -0300

    Desafio 34 versão 2

---
+++ desafio34versao2.py
@@ -0,0 +1,12 @@
+#Desafio 34 versão 2
+#Débora Janini
+
+while True:
+    senha = int(input())
+    if senha != 2002:
+        print('Senha Invalida')
+        
+    else:
+        print('Acesso Permitido')
+        break
+        
